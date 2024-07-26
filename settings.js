1022483b263e6e0a44655f01c919068e55a72288
Lynessy <runataoka@gmail.com>
2024-07-26 07:09:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ function convertToBool(text, fault = 'true') {
 
 global.thumb = fs.readFileSync('./lib/bugs/XByte.png') //Don't change
 //////////////////////////////////////////////////////////
-const BOTNAME = 'X - B Y T E'; //BOT name
+const BOTNAME = 'X - Amnesia; //BOT name
 //////////////////////////////////////////////////////////
 const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
 
@@ -37,7 +37,7 @@ const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
 //----------------OWNER NUMBER------------------------------//
 //////////////////////////////////////////////////////////////////
 
-global.owner = process.env.OWNER_NUMBER  || '923072380380' ;  //put your number without +
+global.owner = process.env.OWNER_NUMBER  || '24107834738' ;  //put your number without +
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -59,7 +59,7 @@ module.exports = { //exporting modules to use in plugins
 //----------------Session ID------------------------------//
 //////////////////////////////////////////////////////////////////
 
-SESSION_ID: process.env.SESSION_ID || 'Paste session id', //Paste your session ID here
+SESSION_ID: process.env.SESSION_ID || 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR1BPZ1hPN2ZnVkZLWXhoWityRDNhV25HSlduT0N3YmZZL0NxQ2pKU0Eycz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicFJsRDBvdDZ2aXRxR3VHYmNvVEpCVktMWHN5S1l2OThBb0Jza05Zb1NsVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxSXNGQXVmWFIxakdKemRhUjNnQXhRVjBYUWVWY1lEam9SYk9iUitvaDJZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwZ3ZCZzAvNUQrZk0rTnM5V2ZhdHlQcmptUml5QzFHbmlHK1dTTDU1WmxjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNBMFNvd1FkUU1KR1ppM1pMNVZIbXhXWWIwb1FjNVYvVkp1aVBzK1NpMnM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVwWlRaaGJCREFvYzdadVVNZzV6ZFNMQ0pCZW1ncHluT2Vsa0RnUlJOaUk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic0VmcllyenlyRFltdGhxdzRaV1h2MHpaeUJtVDRQNFh3aGkvNzV2azMyND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWXlIbi96VlEvMHc5bHdxMnRxazNKbGduc2ZWc0VPSmszUlRLUW45aEpFbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZndXpCZkZTY0IwSHRGMjVTU291bU1QM09IaC9MWCs0REhDd1g3T0ttL3NjSDFKdHJmdGRrR2FWdGt3dmpuVXgxWGdybkZvQTVXM0tnQUpoVnk3c0FBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjQ2LCJhZHZTZWNyZXRLZXkiOiJ5VUVYRGg1QzU2c29wb3RMcDVGNjBhZHZVbmF6eWVHU0hIVllNN2Rpc3hvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiI0TXlDaFh3bFFvNnUzdmJseGJHd1pBIiwicGhvbmVJZCI6ImZjMGQ0MGNmLTI0NDUtNGEzNC04YzBkLTFmNmQ0MTAzZjA5OCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1aGYrZjlMeGw3SCtNbXhMRzFaOTJlVVhSWG89In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidFc3N3o5TzJ4M3ZiN2N2VU1Lb2cwR245ZFdFPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik5IMTVMSDdEIiwibWUiOnsiaWQiOiIyNDEwNzgzNDczODoxNkBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSjJlMU1rR0VLbjJqTFVHR0FzZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiOTBuMW9PUjYwZGV6WURlWlQ0Q214UXhmNlpGT0pYODVrRWFVTytvY3NEOD0iLCJhY2NvdW50U2lnbmF0dXJlIjoiUzN5cGdXNjVPTnZza1V1bUp1TFY5YU9SSk5xTFJFeTZMRmFmVWNwNGg0OWlubUFNN3d6QXIwVTVsa3p3N0dwKzY2aEU1UzdQVTlkS0hkKzc5U0FXQ3c9PSIsImRldmljZVNpZ25hdHVyZSI6InVGRzMrdXJiaFBnc0hoU1ExS01JVHY0czNTTUVTVlhLcTdleXptY1pJM1VMTWxTRVZXMjhzWXNjc0svcTN6cDJkRGViSDJaNEdPVWgyN0RsMUN1cEFRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjQxMDc4MzQ3Mzg6MTZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZmRKOWFEa2V0SFhzMkEzbVUrQXBzVU1YK21SVGlWL09aQkdsRHZxSExBLyJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMTk3MzU1OCwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFMNjIifQ==', //Paste your session ID here
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
